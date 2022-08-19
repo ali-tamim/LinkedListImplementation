b51b67676b8dafaed5afd4959aa238662cefe077
ali tamim <amoh331@gmail.com>
2022-08-19 15:05:00 +0300

    edit ChainNode

--- LinkedList/ChainNode.cs
+++ LinkedList/ChainNode.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace AliTamim.LinkedList
 {
-    public class ChainNode
+    public class ChainNode<T>
     {
-        public int Value { get; set; }
-        public ChainNode? Next { get; set; }
+        public T Value { get; set; }
+        public ChainNode<T>? Next { get; set; }
+
+        public ChainNode(T value)
+        {
+            Value = value;
+        }
     }
 }
