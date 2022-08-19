87f20e97943fb79410141fd14180c684688f98aa
ali tamim <amoh331@gmail.com>
2022-08-19 14:52:49 +0300

    add Chainadd Chain

---
+++ LinkedList/ChainNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliTamim.LinkedList
+{
+    public class ChainNode
+    {
+        public int Value { get; set; }
+        public ChainNode? Next { get; set; }
+    }
+}
