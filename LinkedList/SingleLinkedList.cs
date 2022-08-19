c0e9266684be9c7ea753803cc65fb8607c3a6c97
ali tamim <amoh331@gmail.com>
2022-08-19 15:14:07 +0300

    add SingleLinkedList

---
+++ LinkedList/SingleLinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliTamim.LinkedList
+{
+    public class SingleLinkedList<T> : ICollection<T>
+    {
+        private ChainNode<T>? _head;
+        private ChainNode<T>? _tail;
+
+        #region Add
+        #endregion
+
+        #region remove
+        #endregion
+
+        #region ICollection
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
